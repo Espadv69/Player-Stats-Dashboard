47f048db5eac3501a8f106f637296a4f002d31d5
Espadv69 <aitorjob69@gmail.com>
2025-02-01 15:38:31 +0100

    Add DOM elements

---
+++ index.js
@@ -0,0 +1,15 @@
+// DOM elements
+// Input playerName
+const $playerName = document.querySelector('.playerName')
+// Input player points
+const $points = document.querySelector('.points')
+// Input player assists
+const $assists = document.querySelector('.assists')
+// Input player rebounds
+const $rebounds = document.querySelector('.rebounds')
+// Add Player button
+const $addPlayerButton = document.querySelector('.addPlayer')
+// Got body-table where we will list players
+const $playerList = document.querySelector('.playerList')
+// Got paragraph where we will show the summary
+const $summary = document.querySelector('.summary')
